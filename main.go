59f9260fa9370a1aba7283a53754da7d3967897b
Andrew <andrewbernier42@gmail.com>
2025-03-15 22:39:40 -0400

    Added library for GUI development Currently non-functional

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"andrewbernier42/quizgame/questions"
 	"fmt"
 	"os"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 
+	a := app.New()
+	w := a.NewWindow("Hello Fyne")
+
+	label := widget.NewLabel("Hello, World!")
+	w.SetContent(container.NewVBox(label))
+
+	w.ShowAndRun()
 	problems, err := questions.ReadFile("problems.csv")
 	if err != nil {
 		exit("Failed to parse the provided CSV file.")
